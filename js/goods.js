15fc9459646540424148e489bd9c0e9aeb4a108a
Eugene Parshukov <ev.klit@gmail.com>
2018-10-02 14:38:06 +0300

    add validate card number

--- js/goods.js
+++ js/goods.js
@@ -425,6 +425,39 @@ var priceSlider = function () {
   });
 };
 
+var checkCardNumber = function (inputValue) {
+  var numbers = inputValue.split('').map(function (char, index) {
+    var digit = parseInt(char, 10);
+
+    if (index % 2 === 0) {
+      digit *= 2;
+    }
+
+    return digit > 9 ? digit - 9 : digit;
+  });
+
+  var sum = numbers.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+
+  return sum % 10 === 0;
+};
+
+var cardNumberHandler = function (evt) {
+  var cardInput = evt.target;
+
+  if (cardInput.value.length !== 16) {
+    cardInput.setCustomValidity('Номер карты должен состоять из 16 символов');
+    // return;
+  }
+
+  if (checkCardNumber(cardInput.value)) {
+    cardInput.setCustomValidity('');
+  } else {
+    cardInput.setCustomValidity('Неверный номер кредитной карты');
+  }
+};
+
 hideCatalogLoadedText();
 createGoods();
 toggleTabsDelivery();
@@ -434,3 +467,5 @@ priceSlider();
 document.querySelector('.catalog__cards').addEventListener('click', productHandler);
 
 document.querySelector('.goods__cards').addEventListener('click', cartHandler);
+
+document.querySelector('#payment__card-number').addEventListener('input', cardNumberHandler);
