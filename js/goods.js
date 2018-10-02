9ea365cdd5347d7597a86737ff689076ee626f3e
Eugene Parshukov <ev.klit@gmail.com>
2018-10-02 15:28:00 +0300

    add disable inputs for tabs

--- js/goods.js
+++ js/goods.js
@@ -302,6 +302,9 @@ var removeProductIdFromCart = function (productId) {
 var toggleTabsDelivery = function () {
   var deliverStore = document.querySelector('.deliver__store');
   var deliverCourier = document.querySelector('.deliver__courier');
+  var inputsStore = document.querySelectorAll('.deliver__store input');
+  var textArea = document.querySelectorAll('.deliver__courier textarea');
+  var inputsCourier = document.querySelectorAll('.deliver__courier input');
 
   document.querySelector('.deliver__toggle').addEventListener('click', function (evt) {
     if (!evt.target.id) {
@@ -313,12 +316,17 @@ var toggleTabsDelivery = function () {
 
     var id = evt.target.id;
     document.querySelector('.' + id).classList.remove('visually-hidden');
+    toggleDisableInputs(inputsStore);
+    toggleDisableInputs(inputsCourier);
+    toggleDisableInputs(textArea);
   });
 };
 
 var toggleTabsPayment = function () {
   var paymentCard = document.querySelector('.payment__card-wrap');
   var paymentCash = document.querySelector('.payment__cash-wrap');
+  var inputs = document.querySelectorAll('.payment__inputs input');
+
 
   document.querySelector('.payment__method').addEventListener('click', function (evt) {
     if (!evt.target.id) {
@@ -330,6 +338,7 @@ var toggleTabsPayment = function () {
 
     var id = evt.target.id;
     document.querySelector('.' + id + '-wrap').classList.remove('visually-hidden');
+    toggleDisableInputs(inputs);
   });
 };
 
@@ -456,6 +465,12 @@ var cardNumberHandler = function (evt) {
   } else {
     cardInput.setCustomValidity('Неверный номер кредитной карты');
   }
+};
+
+var toggleDisableInputs = function (inputs) {
+  inputs.forEach(function (input) {
+    input.disabled = !input.disabled;
+  });
 };
 
 hideCatalogLoadedText();
