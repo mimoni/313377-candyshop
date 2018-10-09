d05838584e09e73e0c6af518230660f4444baeba
Eugene Parshukov <ev.klit@gmail.com>
2018-10-09 15:21:06 +0300

    fix price slider

--- js/filter.js
+++ js/filter.js
@@ -1,39 +1,45 @@
 'use strict';
 
 (function () {
-  var RANGE_EL = document.querySelector('.range');
+  var rangeEl = document.querySelector('.range');
 
   var priceSlider = function () {
     var maxPrice = 100;
     var minXPosition = 0;
-    var rangeWidth = RANGE_EL.querySelector('.range__filter').offsetWidth;
-    var priceMinEl = RANGE_EL.querySelector('.range__price--min');
-    var priceMaxEl = RANGE_EL.querySelector('.range__price--max');
-    var rangePinLeftEl = RANGE_EL.querySelector('.range__btn--left');
-    var rangePinRightEl = RANGE_EL.querySelector('.range__btn--right');
+    var rangeWidth = rangeEl.querySelector('.range__filter').offsetWidth;
+    var priceMinEl = rangeEl.querySelector('.range__price--min');
+    var priceMaxEl = rangeEl.querySelector('.range__price--max');
+    var rangePinLeftEl = rangeEl.querySelector('.range__btn--left');
+    var rangePinRightEl = rangeEl.querySelector('.range__btn--right');
     var pinWidth = rangePinLeftEl.offsetWidth;
-    var rangeFillLineEl = RANGE_EL.querySelector('.range__fill-line');
+    var rangeFillLineEl = rangeEl.querySelector('.range__fill-line');
     var getPercentFromRangeBtn = function (el) {
       var percent = parseFloat(el.style.left) * 100 / rangeWidth;
       return Math.round(maxPrice * percent / 100);
     };
 
+    var removeEvent = function (mouseUpHandler, mouseMoveHandler) {
+      document.removeEventListener('mouseup', mouseUpHandler);
+      document.removeEventListener('mousemove', mouseMoveHandler);
+    };
+
     rangePinLeftEl.addEventListener('mousedown', function (evt) {
-      var startCoordinateX = evt.clientX;
+      evt.preventDefault();
+
       var pin = evt.target;
+      var shiftX = evt.clientX - parseInt(pin.style.left, 10);
 
       var mouseMoveHandler = function (moveEvt) {
-        var shiftX = startCoordinateX - moveEvt.clientX;
-
-        startCoordinateX = moveEvt.clientX;
-
         priceMinEl.textContent = getPercentFromRangeBtn(pin);
-        var newPositionPin = parseInt(pin.style.left, 10) - shiftX;
+        var newPositionPin = moveEvt.clientX - shiftX;
 
-        var rightLimiter = parseInt(rangePinRightEl.style.left, 10) - pinWidth / 2;
-        if ((newPositionPin > rightLimiter) || (newPositionPin < minXPosition)) {
-          removeEvents();
-          return;
+        if (newPositionPin < minXPosition) {
+          newPositionPin = 0;
+        }
+
+        var rightLimit = parseInt(rangePinRightEl.style.left, 10) - pinWidth / 2;
+        if (newPositionPin > rightLimit) {
+          newPositionPin = rightLimit;
         }
 
         pin.style.left = newPositionPin + 'px';
@@ -42,43 +48,30 @@
 
       var mouseUpHandler = function () {
         priceMinEl.textContent = getPercentFromRangeBtn(pin);
-        removeEvents();
+        removeEvent(mouseUpHandler, mouseMoveHandler);
       };
 
-      var mouseOutHandler = function () {
-        removeEvents();
-      };
-
-      var removeEvents = function () {
-        pin.removeEventListener('mouseup', mouseUpHandler);
-        pin.removeEventListener('mousemove', mouseMoveHandler);
-        pin.removeEventListener('mousemove', mouseOutHandler);
-      };
-
-      pin.addEventListener('mouseup', mouseUpHandler);
-
-      pin.addEventListener('mousemove', mouseMoveHandler);
-
-      pin.addEventListener('mouseout', mouseOutHandler);
-
+      document.addEventListener('mouseup', mouseUpHandler);
+      document.addEventListener('mousemove', mouseMoveHandler);
     });
 
     rangePinRightEl.addEventListener('mousedown', function (evt) {
-      var startCoordinateX = evt.clientX;
+      evt.preventDefault();
+
       var pin = evt.target;
+      var shiftX = evt.clientX - parseInt(pin.style.left, 10);
 
       var mouseMoveHandler = function (moveEvt) {
-        var shiftX = startCoordinateX - moveEvt.clientX;
-
-        startCoordinateX = moveEvt.clientX;
-
         priceMaxEl.textContent = getPercentFromRangeBtn(pin);
-        var newPositionPin = parseInt(pin.style.left, 10) - shiftX;
+        var newPositionPin = moveEvt.clientX - shiftX;
+
+        if (newPositionPin > rangeWidth) {
+          newPositionPin = rangeWidth;
+        }
 
         var leftLimiter = Math.abs(parseInt(rangePinLeftEl.style.left, 10) + pinWidth / 2);
-        if ((newPositionPin < leftLimiter) || (newPositionPin > rangeWidth)) {
-          removeEvents();
-          return;
+        if (newPositionPin < leftLimiter) {
+          newPositionPin = leftLimiter;
         }
 
         pin.style.left = newPositionPin + 'px';
@@ -87,24 +80,11 @@
 
       var mouseUpHandler = function () {
         priceMaxEl.textContent = getPercentFromRangeBtn(pin);
-        removeEvents();
+        removeEvent(mouseUpHandler, mouseMoveHandler);
       };
 
-      var mouseOutHandler = function () {
-        removeEvents();
-      };
-
-      var removeEvents = function () {
-        pin.removeEventListener('mouseup', mouseUpHandler);
-        pin.removeEventListener('mousemove', mouseMoveHandler);
-        pin.removeEventListener('mousemove', mouseOutHandler);
-      };
-
-      pin.addEventListener('mouseup', mouseUpHandler);
-
-      pin.addEventListener('mousemove', mouseMoveHandler);
-
-      pin.addEventListener('mouseout', mouseOutHandler);
+      document.addEventListener('mouseup', mouseUpHandler);
+      document.addEventListener('mousemove', mouseMoveHandler);
     });
   };
 
