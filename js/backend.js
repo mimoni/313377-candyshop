af0538c5b2c57b91afacb7fe899106af99d23507
Eugene Parshukov <ev.klit@gmail.com>
2018-10-09 15:44:53 +0300

    add download goods from server

---
+++ js/backend.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function () {
+  window.load = function (url, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.open('GET', url);
+    xhr.send();
+  };
+})();
